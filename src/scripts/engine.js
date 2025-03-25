a0d0e2eabbdb181c04e5308a76e85c9779e488b9
SavioFagundes <akirasfm@gmail.com>
2025-03-25 20:05:56 -0300

    Implementação do eventListener de hit

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -8,6 +8,8 @@ const state = {
     values:{
         timerId: null,
         gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
     },
 }
 
@@ -20,14 +22,24 @@ function randomSquare(){
     let randomNumber = Math.floor(Math.random() * 9)
     let randomSquare = state.view.squares[randomNumber]
     randomSquare.classList.add("enemy")
+    state.values.hitPosition = randomSquare.id
 }
 function moveEnemy(){
     state.values.timerId = setInterval(randomSquare, state.values.gameVelocity)
 }
 function addListernerHitBox(){
-    state.view.squares.forEach((square) =>{})
+    state.view.squares.forEach((square) =>{
+        square.addEventListener("mousedown", () =>{
+            if(square.id === state.values.hitPosition){
+                state.values.result ++
+                state.view.score.textContent = state.values.result
+                state.values.hitPosition = null;
+            }
+        })
+    })
 }
 function initialize(){
     moveEnemy()
+    addListernerHitBox()
 }
 initialize()
